8e52c81688247abeff85c7cf681bbbdfc746b614
Pedzisai Poseni <pedzisaiposeni@gmail.com>
2025-11-18 21:56:01 +0200

    Fixed analytics page

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { usePageTitle } from '../hooks/usePageTitle';
 import { collection, query, where, getDocs } from "firebase/firestore";
